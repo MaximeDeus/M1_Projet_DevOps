eb08bb16ec645c56772896621c8e23aae398272d
MaximeDeus <maxime.deroissart@etu.univ-grenoble-alpes.fr>
2019-04-06 20:26:56 +0200

    Création de deux classes d'exception qui permettent de limiter les cas d'usages

---
+++ projet.devops.deroissart.defours/src/main/java/fr/uga/devops/panda/exception/NotALabelException.java
@@ -0,0 +1,7 @@
+package fr.uga.devops.panda.exception;
+
+public class NotALabelException extends Exception {
+    public NotALabelException(String message) {
+        super(message);
+    }
+}
---
+++ projet.devops.deroissart.defours/src/main/java/fr/uga/devops/panda/exception/NotAnIntegeException.java
@@ -0,0 +1,8 @@
+package fr.uga.devops.panda.exception;
+
+public class NotAnIntegeException extends Exception {
+
+    public NotAnIntegeException(String message) {
+        super(message);
+    }
+}
